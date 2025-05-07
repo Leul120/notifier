2745b4a1ee128f76ceca309b46d60a005dcb7e65
Leul120 <140542389+Leul120@users.noreply.github.com>
2025-05-07 14:48:37 -0700

    Update UserRepository.java

--- src/main/java/com/safeStatusNotifier/safeStatusNotifier/repositories/UserRepository.java
+++ src/main/java/com/safeStatusNotifier/safeStatusNotifier/repositories/UserRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import java.util.UUID;
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
     User findByRole(Role role);
