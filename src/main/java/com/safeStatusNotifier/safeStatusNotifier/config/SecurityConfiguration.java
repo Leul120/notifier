a8f58428a53fe7a2da097b7b3b0c0dc4acf2bb72
Leul120 <140542389+Leul120@users.noreply.github.com>
2025-10-12 05:10:42 -0700

    Update SecurityConfiguration.java

--- src/main/java/com/safeStatusNotifier/safeStatusNotifier/config/SecurityConfiguration.java
+++ src/main/java/com/safeStatusNotifier/safeStatusNotifier/config/SecurityConfiguration.java
@@ -37,8 +37,8 @@ public class SecurityConfiguration {
         try {
             http.csrf(AbstractHttpConfigurer::disable)
                     .authorizeHttpRequests(request -> request
-                            .requestMatchers("/api/auth/**","/ws/**").permitAll()
-                            .requestMatchers("/ws/**").permitAll()
+                            .requestMatchers("/api/auth/**").permitAll()
+//                            .requestMatchers("/ws/**").permitAll()
 //                            .requestMatchers("/api/v1/admin/**").hasAnyAuthority(Role.ADMIN.name())
 //                            .requestMatchers("/api/v1/user/**").hasAnyAuthority(Role.USER.name())
                             .anyRequest().authenticated())
